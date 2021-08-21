a510191d0245aa69e73d2e2c0f0d600c021b2855
insinfo <insinfo2008@gmail.com>
2021-08-20 22:31:22 -0300

    fix windows high CPU usage

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 /*import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:fsbackup/main2.dart';
+import 'package:fsbackup/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
@@ -27,5 +27,4 @@ void main() {
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
-}
-*/
+}*/

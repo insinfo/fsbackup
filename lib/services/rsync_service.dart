594362570a2004a646ed16a92d6304061ef22bfa
insinfo <insinfo2008@gmail.com>
2021-08-31 17:53:19 -0300

    inicio dos testes no lado linux

--- lib/services/rsync_service.dart
+++ lib/services/rsync_service.dart
@@ -1,11 +1,16 @@
-//C:\Program Files (x86)\ICW\bin\cygrunsrv.exe
+//usuario e senha do copssh
+//user: SvcCOPSSH
+//senha: tibMKCp7xJ3787
+//C:\Program Files (x86)\ICW\bin\cygrunsrv.exe | OpenSSHServer
+//usuario e senha do cwRsyncServer
 //user of cwRsyncServer
 //User: SvcCWRSYNC
 //Senha: tibMKCp7xJ3787
 //Add new user account from command line (CMD)
 //net user root 257257 /ADD
 //net localgroup administrators root /add
-//cd ICW\bin
+//Em seguida digite os seguintes comandos que estão dentro de c:\ICW\bin
+//Esses comandos basicamente irão ler a base local de usuários e grupos do Windows e ao mesmo tempo vão direcionar a saída para os arquivos passwd e group para o diretório C:\ICW\etc.
 //mkpasswd.exe -l > ..\etc\passwd
 //mkgroup.exe -l > ..\etc\group
 //C:\Program Files (x86)\ICW\rsyncd.conf
@@ -22,5 +27,25 @@
 //rsync -Cravz --chmod=u=rwx,g=rx,o=rx --progress --partial --delete-excluded /var/www/dart/$(date +%Y%m%d).tar.gz rsync://192.168.66.123/bkp
 //ou
 //rsync -aix --progress /var/www/dart/$(date +%Y%m%d).tar.gz rsync://192.168.66.123/bkp
+//usando rsync sobre SSH
+// rsync -aix --progress /var/www/dart/$(date +%Y%m%d).tar.gz isaque@192.168.66.123:/cygdrive/c/bkp/
 
+//chave publica id_rsa.pub
+//-----BEGIN EC PRIVATE KEY-----
+//MHcCAQEEIPDZdjsKAaXY4ZitrxpJNlfsPAjorDVLMUePsj7IAcqCoAoGCCqGSM49
+//AwEHoUQDQgAEyb4HWPzNXrR7Rj207pyrWC2lGCOvuJjvEKUcE1s1C5gqY1W2qXqC
+//hEeAlUaiLsK5Uw/IlHI4uKug5KfeSQzPZQ==
+//-----END EC PRIVATE KEY-----
+
+//chmod 600 /var/www/dart/id_rsa.pub
+//copia o arquivo usando a chave publica
+//  rsync --progress -ave  "ssh -i /var/www/dart/id_rsa.pub" /var/www/dart/$(date +%Y%m%d).tar.gz isaque@192.168.66.123:/cygdrive/c/bkp/
+
+//Baseado em Berkeley Software Distribution (BSD) Remote Copy Protocol, o SCP (Secure Copy) é um protocolo de rede para transferências de arquivos
+//Na essência, o SCP é uma mistura de RCP e SSH (Secure Shell)
+//scp <source> <destination>
+//To copy a file from B to A while logged into B:
+//scp /var/www/dart/laravel.txt  isaque@192.168.66.123:/c/bkp/
+//To copy a file from B to A while logged into A:
+//scp username@b:/path/to/file /path/to/destination
 class RsyncService {}
